9d1934396c8aa63b5e16aabb4b6f2773e03d6e45
Kristoffer Tonning <tonning@gmail.com>
2023-04-02 14:26:05 +0200

    Add ability set a countdown timer

--- resources/views/settings/types/inline.blade.php
+++ resources/views/settings/types/inline.blade.php
@@ -1,34 +1,92 @@
-<x-fab::forms.input
-    label="Content"
-    class="mt-6"
-    name="payload[content]"
-    wire:model="payload.content"
-    wire:key="promobar_content"
-/>
-
-<div class="flex mt-6">
-    <x-fab::forms.input
-        label="Background color"
-        class="mr-4"
-        type="color"
-        name="payload[background_color]"
-        wire:model="payload.background_color"
-        wire:key="promobar_background_color"
-    />
-
-    <x-fab::forms.input
-        label="Text color"
-        class="ml-4"
-        type="color"
-        name="payload[text_color]"
-        wire:model="payload.text_color"
-        wire:key="promobar_text_color"
-    />
-</div>
-
-<x-fab::layouts.panel
-    title="Preview"
-    class="mt-8"
+<div
+    x-data="{ payload: $wire.entangle('payload') }"
+    x-init="if (typeof(payload.countdown_timer_enabled) == 'undefined') payload.countdown_timer_enabled = false;"
 >
-    @include('promobar::types.inline')
-</x-fab::layouts.panel>
+    <x-fab::forms.input
+        label="Content"
+        class="mt-6"
+        name="payload[content]"
+        wire:model="payload.content"
+        wire:key="promobar_content"
+    />
+
+    <div class="flex mt-6">
+        <x-fab::forms.input
+            label="Background color"
+            class="mr-4"
+            type="color"
+            name="payload[background_color]"
+            wire:model="payload.background_color"
+            wire:key="promobar_background_color"
+        />
+
+        <x-fab::forms.input
+            label="Text color"
+            class="ml-4"
+            type="color"
+            name="payload[text_color]"
+            wire:model="payload.text_color"
+            wire:key="promobar_text_color"
+        />
+
+        <x-fab::forms.input
+            label="Padding"
+            class="ml-4"
+            type="number"
+            min="0"
+            name="payload[padding]"
+            wire:model="payload.padding"
+            wire:key="promobar_padding"
+            placeholder="10"
+            trailing="px"
+        />
+    </div>
+
+    <div class="flex flex-col mt-6 items-start space-y-4 mt-8">
+        <x-fab::forms.toggle
+            id="enable_countdown_timer"
+            label="Enable countdown timer"
+            wire:model="payload.countdown_timer_enabled"
+            wire:key="promobar_countdown_timer_enabled"
+            :toggled="$payload['countdown_timer_enabled'] ?? false"
+        />
+
+        <div
+            class="grid grid-cols-2 w-full fab-space-x-4"
+            x-cloak
+            x-show="payload.countdown_timer_enabled === true"
+        >
+            <x-fab::forms.date-picker
+                label="Count down until"
+                wire:model="payload.countdown_timer_end_date"
+                wire:key="promobar_countdown_timer_end_date"
+                hint="Required"
+                :options="[
+                    'altInput' => true,
+                    'altFormat' => 'D, M J, Y - G:i K',
+                    'enableTime' => true
+                ]"
+            />
+
+            <x-fab::forms.date-picker
+                label="Show timer after"
+                wire:model="payload.countdown_timer_start_date"
+                wire:key="promobar_countdown_timer_start_date"
+                hint="Optional"
+                :options="[
+                    'altInput' => true,
+                    'altFormat' => 'D, M J, Y - G:i K',
+                    'enableTime' => true
+                ]"
+            />
+
+        </div>
+    </div>
+
+    <x-fab::layouts.panel
+        title="Preview"
+        class="mt-8"
+    >
+        @include('promobar::types.inline')
+    </x-fab::layouts.panel>
+</div>
--- resources/views/types/inline.blade.php
+++ resources/views/types/inline.blade.php
@@ -1,14 +1,26 @@
 <style>
     .astrogoat_promobar {
-        padding: 10px;
+        display: flex;
+        justify-content: center;
+        padding: {{ isset($payload['padding']) && ! blank($payload['padding']) ? $payload['padding'] : '10' }}px;
         text-align: center;
         color: {{ $payload['text_color'] ?? '#000' }};
         background-color: {{ $payload['background_color'] ?? '#FFF' }};
     }
+
+    .astrogoat_promobar_countdown {
+        margin-left: 10px;
+    }
 </style>
 
 @isset($payload['content'])
-    <div class="astrogoat_promobar">
+    <div
+        class="astrogoat_promobar"
+    >
         {!! $payload['content'] ?? '' !!}
+
+        @if($payload['countdown_timer_enabled'] ?? false)
+            <x-promobar::countdown :payload="$payload" class="astrogoat_promobar_countdown" />
+        @endif
     </div>
 @endisset
